b1179182248e5ab64c6c688b2289ba691de98c5d
Bouali <youssefbouali206@gmail.com>
2024-12-28 00:58:55 +0100

    Add Front in this branch and resolve postcss

---
+++ app/Controllers/LoginController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\UserModel;
+use App\Models\AccountModel;
+use CodeIgniter\Controller;
+
+class LoginController extends Controller
+{
+    
+    // Show the login form
+    public function loginForm()
+    {
+        return view('login/form'); 
+    }
+
+    // Handle user login
+    public function login()
+    {
+        
+        $accountModel = new AccountModel();
+
+        
+        $email = $this->request->getPost('email');
+        $password = $this->request->getPost('password');
+
+        
+        if (empty($email) || empty($password)) {
+            return redirect()->back()->with('erreur');
+        }
+
+        /* Check if the user exists based on email (assuming email is unique)
+        $user = $userModel->where('email', $email)->first();
+
+        if (!$user) {
+            return redirect()->back()->with('error', 'User not found.');
+        }*/
+
+        
+        if (!password_verify($password, $user['password'])) {
+            return redirect()->back()->with('error', 'Mot de passe incorrect');
+        }
+
+        // Set session data (you can store more user data here)
+        session()->set([
+            'user_id' => $user['id'],
+            'email' => $user['email'],
+            'logged_in' => true
+        ]);
+
+        
+        return redirect()->to('/users/Accueil'); 
+    }
+
+    
+    public function logout()
+    {
+        
+        session()->destroy();
+
+       
+        return redirect()->to('/login');
+    }
+}
