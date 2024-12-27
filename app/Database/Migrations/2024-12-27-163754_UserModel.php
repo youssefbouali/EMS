91080f5bf4019e448f019d88b7c832732a3334ce
Bouali <youssefbouali206@gmail.com>
2024-12-27 17:43:02 +0100

    add role model

--- app/Database/Migrations/2024-12-27-163754_UserModel.php
+++ app/Database/Migrations/2024-12-27-163754_UserModel.php
@@ -4,15 +4,44 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class UserModel extends Migration
+class CreateUserTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nom' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'prenom' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'numEtudiant' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 50,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('user');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('user');
     }
 }
