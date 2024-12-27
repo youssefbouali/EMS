b86f84ad7916afa3b70bdf7f178021e59c57e894
Abdelhadi Slimane <sliwabdel15@gmail.com>
2024-12-27 14:36:44 +0100

    front commit

---
+++ web/ESM/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500 text-white">
+      <h1 className="text-4xl font-bold">Hello TailwindCSS!</h1>
+    </div>
+  );
+}
